8dadee426037f7f2ee7b3729421896b8115207d6
barko99 <42874667+barko99@users.noreply.github.com>
2018-10-25 10:18:43 +0200

    Update race_session_event_adapter.rb

--- web/app/adapters/race_session_event_adapter.rb
+++ web/app/adapters/race_session_event_adapter.rb
@@ -28,15 +28,20 @@ class RaceSessionEventAdapter
     puts "pilot_num_tracked_laps: #{pilot_num_tracked_laps}"
     puts "max_laps_for_this_race: #{max_laps_for_this_race}"
 
-    if(max_laps_for_this_race > pilot_num_tracked_laps)
+    if((max_laps_for_this_race - 1) > pilot_num_tracked_laps)
       # play the lap announcement for the last tracked lap count for this pilot
       puts "RaceSessionEventAdapter::perform_for_competition_mode: triggering sound for lap #{pilot_num_tracked_laps}"
       RaceLapAnnouncerWorker.perform_async(pilot_num_tracked_laps)
+    else if((max_laps_for_this_race - 1) == pilot_num_tracked_laps)
+      # play the lap announcement for the last tracked lap count for this pilot
+      puts "RaceSessionEventAdapter::perform_for_competition_mode: triggering sound for lap #{pilot_num_tracked_laps}"
+      RaceLapAnnouncerWorker.perform_async(pilot_num_tracked_laps)
+      SoundFileWorker.perform_async("sfx_lap_final_round")
     else
       #pilot finished race... let's see
       # RaceSessionEventAdapter.new(RaceSessionAdapter.new(RaceSession.find(6)),12).perform
       puts "finished race"
-
+      SoundFileWorker.perform_async("sfx_race_finished")
       data = PilotRaceLap.where(race_session_id: self.race_session_adapter.race_session.id).where(lap_num: max_laps_for_this_race)
 
       listing =  self.race_session_adapter.listing
